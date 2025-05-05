b79caff165f19bca657a920ff87265405b012e42
zekainhuman1 <zekainhuman@gmail.com>
2025-05-05 12:58:26 +0000

    front

--- src/solana-service/program.ts
+++ src/solana-service/program.ts
@@ -6,6 +6,11 @@ import escrowIdl from "./escrow.json";
 import { Escrow } from "./idlType";
 import { config } from "./config";
 
+import { getAssociatedTokenAddressSync, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { randomBytes } from "crypto";
+const TOKEN_PROGRAM = TOKEN_PROGRAM_ID
+
+
 export class EscrowProgram {
   protected program: Program<Escrow>;
   protected connection: web3.Connection;
@@ -38,8 +43,51 @@ export class EscrowProgram {
     tokenAmountB: number
   ) {
     try {
-      console.log(tokenMintA, tokenMintB, tokenAmountA, tokenAmountB);
-      return null;
+      const offerId = new BN(randomBytes(8));
+      const offerAddress = this.createOfferId(offerId);
+      const vault = getAssociatedTokenAddressSync(
+        tokenMintA,
+        offerAddress,
+        true,
+        TOKEN_PROGRAM
+      );
+      const makerTokenAccountA = getAssociatedTokenAddressSync(
+        tokenMintA,
+        this.wallet.publicKey,
+        true,
+        TOKEN_PROGRAM
+      );
+      const makerTokenAccountB = getAssociatedTokenAddressSync(
+        tokenMintB,
+        this.wallet.publicKey,
+        true,
+        TOKEN_PROGRAM
+      );
+      const accounts = {
+        maker: this.wallet.publicKey,
+        tokenMintA: tokenMintA,
+        makerTokenAccountA,
+        tokenMintB: tokenMintB,
+        makerTokenAccountB,
+        vault,
+        offer: offerAddress,
+      };
+      const txInstruction = await this.program.methods
+        .makeOffer(offerId, new BN(tokenAmountA), new BN(tokenAmountB))
+        .accounts({ ...accounts, tokenProgram: TOKEN_PROGRAM })
+        .instruction();
+      const messageV0 = new web3.TransactionMessage({
+        payerKey: this.wallet.publicKey,
+        recentBlockhash: (await this.connection.getLatestBlockhash()).blockhash,
+        instructions: [txInstruction],
+      }).compileToV0Message();
+      const versionedTransaction = new web3.VersionedTransaction(messageV0);
+      if (!this.program.provider.sendAndConfirm) return;
+      const response = await this.program.provider.sendAndConfirm(
+        versionedTransaction
+      );
+      if (!this.program.provider.publicKey) return;
+      return response;
     } catch (e) {
       console.log(e);
       return null;
@@ -53,8 +101,57 @@ export class EscrowProgram {
     tokenMintB: PublicKey
   ) {
     try {
-      console.log(maker, offer, tokenMintA, tokenMintB);
-      return null;
+      const takerTokenAccountA = getAssociatedTokenAddressSync(
+        tokenMintA,
+        this.wallet.publicKey,
+        true,
+        TOKEN_PROGRAM
+      );
+      const takerTokenAccountB = getAssociatedTokenAddressSync(
+        tokenMintB,
+        this.wallet.publicKey,
+        true,
+        TOKEN_PROGRAM
+      );
+      const makerTokenAccountB = getAssociatedTokenAddressSync(
+        tokenMintB,
+        maker,
+        true,
+        TOKEN_PROGRAM
+      );
+      const vault = getAssociatedTokenAddressSync(
+        tokenMintA,
+        offer,
+        true,
+        TOKEN_PROGRAM
+      );
+      const accounts = {
+        maker,
+        offer,
+        taker: this.wallet.publicKey,
+        takerTokenAccountA,
+        takerTokenAccountB,
+        vault,
+        tokenProgram: TOKEN_PROGRAM,
+        makerTokenAccountB,
+      };
+      const txInstruction = await this.program.methods
+        .takeOffer()
+        .accounts({
+          ...accounts,
+        })
+        .instruction();
+      const messageV0 = new web3.TransactionMessage({
+        payerKey: this.wallet.publicKey,
+        recentBlockhash: (await this.connection.getLatestBlockhash()).blockhash,
+        instructions: [txInstruction],
+      }).compileToV0Message();
+      const versionedTransaction = new web3.VersionedTransaction(messageV0);
+      if (!this.program.provider.sendAndConfirm) return;
+      const response = await this.program.provider.sendAndConfirm(
+        versionedTransaction
+      );
+      return response;
     } catch (e) {
       console.log(e);
       return null;
